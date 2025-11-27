dc3a894783cd97ade1b3e0130682864205e28ff5
Nkemjika1Divine <cndivine@gmail.com>
2025-11-27 22:51:12 +0100

    category model added

---
+++ backend/models/category.py
@@ -0,0 +1,10 @@
+from models.basemodel import Basemodel, Base
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, func, ForeignKey
+
+
+class Category(Basemodel, Base):
+    __tablename__ = "categories"
+    name = Column(String(20), nullable=False)
+    user_id = Column(
+        String(50), ForeignKey("users.id", ondelete="CASCADE"), nullable=True
+    )
