becb91f962775d0ca48967b300478325c060b447
Nkemjika1Divine <cndivine@gmail.com>
2025-11-28 01:03:59 +0100

    income model added

--- backend/models/income.py
+++ backend/models/income.py
@@ -13,20 +13,16 @@ from sqlalchemy import (
 from datetime import datetime
 
 
-class BudgetPeriod(Enum):
-    monthly = "monthly"
-    weekly = "weekly"
-
-
-class Budget(Basemodel, Base):
-    __tablename__ = "budgets"
+class Income(Basemodel, Base):
+    __tablename__ = "incomes"
 
     user_id = Column(
         String(50), ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
     category_id = Column(
-        String(50), ForeignKey("categories.id", ondelete="CASCADE"), nullable=False
+        String(50), ForeignKey("categories.id", ondelete="CASCADE"), nullable=True
     )
-    amount_limit = Column(Float, nullable=False)
-    period = Column(String(10), nullable=False)
+    amount = Column(Float, nullable=False)
+    description = Column(String(200))
+    timestamp = Column(DateTime, nullable=True)
     is_deleted = Column(Boolean, default=False)
