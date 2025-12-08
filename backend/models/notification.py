c6b2a92462bfdd6ae83e2eef3d36009c3758114b
Nkemjika1Divine <cndivine@gmail.com>
2025-12-08 20:14:20 +0100

    Notifications model created

--- backend/models/notification.py
+++ backend/models/notification.py
@@ -1,21 +1,22 @@
+from enum import Enum
 from models.basemodel import Basemodel, Base
 from sqlalchemy import (
     Column,
+    Integer,
     String,
     DateTime,
     Boolean,
     ForeignKey,
     Float,
 )
+from datetime import datetime
 
 
-class Income(Basemodel, Base):
-    __tablename__ = "incomes"
+class Notification(Basemodel, Base):
+    __tablename__ = "notifications"
 
     user_id = Column(
         String(50), ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
-    amount = Column(Float, nullable=False)
-    description = Column(String(200))
-    timestamp = Column(DateTime, nullable=False)
-    is_deleted = Column(Boolean, default=False)
+    notification = Column(String(500), nullable=False)
+    is_read = Column(Boolean, default=False)
